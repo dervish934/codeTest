8232c09206041841496968e6bef227d7c8af829b
Josue <jortigoza@dreidel.mx>
2020-02-14 10:44:05 -0600

    Avances en el juego del hacker

---
+++ Assets/WM2000/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine("HELLO, THIS IS DREIDEL INTERACTIVE");
+        Terminal.WriteLine("WHAT WOULD YOU LIKE TO DO?");
+
+        Terminal.WriteLine("-Tinos");
+        Terminal.WriteLine("-Bingos");
+        Terminal.WriteLine("-Slots");
+
+        Terminal.WriteLine("Enter your selection:");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
